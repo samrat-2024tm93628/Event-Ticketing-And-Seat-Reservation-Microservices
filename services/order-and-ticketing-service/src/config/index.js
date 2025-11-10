092a5cf43136191c1686119d6cafcc4a7ee264b8
iamsamratsingh <samrat@rapidops.com>
2025-11-10 16:26:15 +0530

    fix iunter service call.

--- services/order-and-ticketing-service/src/config/index.js
+++ services/order-and-ticketing-service/src/config/index.js
@@ -9,7 +9,7 @@ const config = {
     reservationServiceUrl: process.env.RESERVATION_SERVICE_URL || 'http://localhost:3002/v1/seats',
     paymentServiceUrl: process.env.PAYMENT_SERVICE_URL || 'http://localhost:3003',
     userServiceUrl: process.env.USER_SERVICE_URL || 'http://localhost:3004/v1/users',
-    catalogServiceUrl: process.env.CATALOG_SERVICE_URL || 'http://localhost:3005/v1/events'
+    catalogServiceUrl: process.env.CATALOG_SERVICE_URL || 'http://localhost:3005'
   },
   jwt: {
     secret: process.env.JWT_SECRET || 'dev_secret_key',
